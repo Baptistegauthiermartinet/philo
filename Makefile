8308dcccb750758ae4673b3756b5240f09a55704
Baptiste Gauthier-Martinet <bgauthie@z1r11p1.42lyon.fr>
2023-10-20 15:13:40 +0200

    start

---
+++ Makefile
@@ -0,0 +1,37 @@
+NAME = philo
+CC = cc -Wall -Werror -Wextra -g3
+RM = rm -rf
+
+SRC_PATH = ./src/
+SRC_FILE = 	main.c
+SRC = $(addprefix $(SRC_PATH), $(SRC_FILE))
+
+OBJ_PATH = ./obj/
+OBJ_FILE = $(SRC:.c=.o)
+OBJ = $(addprefix $(OBJ_PATH), $(OBJ_FILE))
+
+INCLUDE_PATH = ./include/
+INCLUDE_FILE = philo.h
+INC = $(addprefix $(INCLUDE_PATH), $(INCLUDE_FILE))
+
+all : $(NAME)
+
+$(NAME) : $(OBJ_PATH) $(OBJ)
+	$(CC) $(OBJ) -o $(NAME)
+
+$(OBJ_PATH)%.o : %.c $(INC)
+	mkdir -p $(shell dirname $@)
+	$(CC) -c $< -o $@
+
+$(OBJ_PATH) :
+	mkdir $@
+
+clean :
+	$(RM) $(OBJ_PATH)
+
+fclean : clean
+	$(RM) $(NAME)
+
+re : fclean all
+
+.PHONY : all clean fclean re
---
+++ include/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgauthie <bgauthie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 14:30:16 by bgauthie          #+#    #+#             */
+/*   Updated: 2023/10/20 14:58:12 by bgauthie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/*printf*/
+# include <stdio.h>
+
+/*memset*/
+# include <string.h>
+
+/*malloc*/
+# include <stdlib.h>
+
+/*write, usleep*/
+# include <unistd.h>
+
+/*gettimeofday*/
+# include <sys/time.h>
+
+/*pthread_create, pthread_detach, pthread_join, pthread_mutex_init,
+	pthread_mutex_lock, pthread_mutex_unlock, pthread_mutex_destroy*/
+# include <pthread.h>
+
+
+
+#endif
